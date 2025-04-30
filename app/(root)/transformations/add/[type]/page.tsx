cca727ed5b5b6af1147ccddf33f93ab12bf8e2c9
Ibrahims168 <96667811+Ibrahims168@users.noreply.github.com>
2025-04-30 21:48:02 +0300

    update

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,44 +1,36 @@
+import Header from '@/components/shared/Header'
+import TransformationForm from '@/components/shared/transformationForm';
+import { transformationTypes } from '@/constants'
+// import { getUserById } from '@/lib/actions/user.actions';
 import { auth } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
-import Header from '@/components/shared/Header';
-import TransformationForm from '@/components/shared/transformationForm';
-import { transformationTypes } from '@/constants';
-import { getUserById } from '@/lib/actions/user.actions';
 
-const AddTransformationTypePage = async ({
-  params,
-}: {
-  params: { type: TransformationTypeKey };
-}) => {
+const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
   const { userId } = await auth();
+  const transformation = transformationTypes[type];
 
-  const transformation = transformationTypes[params.type];
+  if(!userId) redirect('/sign-in')
 
-  if (!userId) redirect('/sign-in');
-
-  const user = await getUserById(userId);
-  if (!user) {
-    console.error('User not found for ID:', userId);
-    redirect('/sign-up');
-  }
+  // const user = await getUserById(userId);
 
   return (
     <>
-      <Header
+      <Header 
         title={transformation.title}
         subtitle={transformation.subTitle}
       />
-
-      <section className="mt-10">
-        <TransformationForm
+    
+      {/* <section className="mt-10">
+        <TransformationForm 
           action="Add"
           userId={user._id}
           type={transformation.type as TransformationTypeKey}
           creditBalance={user.creditBalance}
         />
-      </section>
+      </section> */}
+      
     </>
-  );
-};
+  )
+}
 
-export default AddTransformationTypePage;
+export default AddTransformationTypePage
