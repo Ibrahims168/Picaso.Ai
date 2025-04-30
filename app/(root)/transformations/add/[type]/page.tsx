e0f164698ce63d1a68b95d5913cfb2b1ec335514
Ibrahims168 <96667811+Ibrahims168@users.noreply.github.com>
2025-04-30 21:41:39 +0300

    update

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,31 +1,38 @@
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 import Header from '@/components/shared/Header';
 import TransformationForm from '@/components/shared/transformationForm';
 import { transformationTypes } from '@/constants';
 import { getUserById } from '@/lib/actions/user.actions';
-import { auth } from '@clerk/nextjs/server';
-import { redirect } from 'next/navigation';
 
-const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
+type SearchParamProps  = {
+  params: {
+    type: TransformationTypeKey;
+  };
+};
+
+const AddTransformationTypePage = async ({ params }: SearchParamProps ) => {
   const { userId } = await auth();
-  const transformation = transformationTypes[type];
 
-  if(!userId) redirect('/sign-in')
+  const transformation = transformationTypes[params.type];
 
-    const user = await getUserById(userId);
-    if (!user) {
-      console.error('User not found for ID:', userId);
-      redirect('/sign-up'); // Redirect to sign-up if user doesn't exist
-    }
-  
+  if (!userId) redirect('/sign-in');
+
+  const user = await getUserById(userId);
+  if (!user) {
+    console.error('User not found for ID:', userId);
+    redirect('/sign-up');
+  }
+
   return (
     <>
-      <Header 
+      <Header
         title={transformation.title}
         subtitle={transformation.subTitle}
       />
-    
+
       <section className="mt-10">
-        <TransformationForm 
+        <TransformationForm
           action="Add"
           userId={user._id}
           type={transformation.type as TransformationTypeKey}
@@ -33,8 +40,7 @@ const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps)
         />
       </section>
     </>
-  )
-}
-
+  );
+};
 
 export default AddTransformationTypePage;
