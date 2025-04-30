b0e398af7fd53d8119fb278090a015ffa77a01f3
Ibrahims168 <96667811+Ibrahims168@users.noreply.github.com>
2025-04-30 20:42:18 +0300

    update

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,38 +1,11 @@
-// import Header from '@/components/shared/Header'
-// import TransformationForm from '@/components/shared/transformationForm';
-// import { transformationTypes } from '@/constants'
-// import { getUserById } from '@/lib/actions/user.actions';
-// import { auth } from '@clerk/nextjs/server';
-// import { redirect } from 'next/navigation';
+import React from 'react'
 
+const page = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
 
-
-// const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
-//   const { userId } = await auth();
-//   const transformation = transformationTypes[type];
-//   console.log("USER ID CREATED : " + userId)
-  
-//   if(!userId) redirect('/sign-in')
-
-//   const user = await getUserById(userId);
-
-//   return (
-    
-//       <Header 
-//         title={transformation.title}
-//         subtitle={transformation.subTitle}
-//       />
-    
-//       <section className="mt-10">
-//         <TransformationForm 
-//           action="Add"
-//           userId={user._id}
-//           type={transformation.type as TransformationTypeKey}
-//           creditBalance={user.creditBalance}
-//         />
-//       </section>
-//     </>
-//   )
-// }
-
-// export default AddTransformationTypePage
+export default page
